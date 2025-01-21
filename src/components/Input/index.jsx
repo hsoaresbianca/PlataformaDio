b943d5cec4898599170e6693c66b86ed98941747
Bianca Soares <biasoares.bianca@gmail.com>
2025-01-21 12:35:36 -0300

    Error Message no input
    incluindo o error message no input

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import { Controller } from "react-hook-form";
 
-import {InputContainer, InputText, IconContainer } from './styles';
+import {InputContainer, InputText, IconContainer, ErrorText } from './styles';
 
-const Input = ({leftIcon, name, control, ...rest}) => {
-
+const Input = ({leftIcon, name, control, errorMessage, ...rest}) => {
 
   return (
-    <InputContainer>
-        {leftIcon ? (<IconContainer>{leftIcon}</IconContainer>) : null}
-        <Controller
-          name={name}
-          control={control}
-          render={({ field }) =>  <InputText {...field} {...rest} />}
-        />
-       
-    </InputContainer>
+    <>
+      <InputContainer>
+          {leftIcon ? (<IconContainer>{leftIcon}</IconContainer>) : null}
+          <Controller
+            name={name}
+            control={control}
+            render={({ field }) =>  <InputText {...field} {...rest} />}
+          />  
+      </InputContainer>
+      {errorMessage ? <ErrorText>{errorMessage}</ErrorText> : null}
+    </>
   )
 }
 
