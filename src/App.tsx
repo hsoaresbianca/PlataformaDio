ac809cdd526108c7fbd25b9475b5bfc52fa0871b
Bianca Soares <biasoares.bianca@gmail.com>
2025-03-12 11:24:11 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,20 @@ import { Feed } from "./pages/feed";
 import { Home } from './pages/home'
 import { Login } from './pages/login'
 import { GlobalStyle } from './styles/global';
+import { AuthContextProvider } from "./context/auth";
 
 function App() {
   return (
-    <Router>
-     <GlobalStyle />
-     <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/feed" element={<Feed />} />
-     </Routes >
-    </Router>
+    <AuthContextProvider>
+      <Router>
+      <GlobalStyle />
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/feed" element={<Feed />} />
+      </Routes >
+      </Router>
+    </AuthContextProvider>
   );
 }
 
